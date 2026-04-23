5ae146545cbc6af844928cc71fe7ad12ef138cbc
marcosgabrielgreco <165661707+marcosgabrielgreco@users.noreply.github.com>
2026-04-23 09:46:04 -0300

    s

--- js main.js
+++ js main.js
@@ -1,2 +1,28 @@
-const botoes = document.querySelectorAll(".botao")
-console.log(botoes);
+//aula 4 (substituir a aula 2)
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+
+for(let i=0;i <botoes.length;i++){
+botoes[i].onclick = function(){
+
+
+for(let j=0;j<botoes.length;j++){
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+}
+
+
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+}
+}
+
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-04-20T00:00:00");
+let tempoAtual = new Date();
+
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
